880361954241943d162aaf781ed63217018d3c17
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2023-04-17 22:33:27 +0900

    Update ABC_analysis.py

--- ABC_analysis.py
+++ ABC_analysis.py
@@ -5,6 +5,7 @@ import itertools
 from google.colab import files
 import matplotlib.pyplot as plt
 import seaborn as sns
+from google.colab import drive
 
 class ABC_analysis:
   def __init__(self, url, string_type, myfile):
@@ -19,21 +20,21 @@ class ABC_analysis:
       try: 
         df = pd.read_excel(io.BytesIO(self.myfile[self.url]))
       except:
-        df = pd.read_excel('/content/logistic/재고_DATA.xlsx')
+        df = pd.read_excel('/content/재고_DATA.xlsx')
 
 
     if self.string_type == "release":
       try: 
         df = pd.read_excel(io.BytesIO(self.myfile[self.url]))
       except:
-        df = pd.read_excel('/content/logistic/출고_DATA.xlsx')
+        df = pd.read_excel('/content/출고_DATA.xlsx')
 
 
     if self.string_type == "order":
       try: 
         df = pd.read_excel(io.BytesIO(self.myfile[self.url]))
       except:
-        df = pd.read_excel('/content/logistic/주문_DATA.xlsx')
+        df = pd.read_excel('/content/주문_DATA.xlsx')
 
 
     return df
@@ -45,8 +46,6 @@ class ABC_analysis:
       df = df.sort_values(by='일평균 재고수량',ascending=False)
       #'일평균 재고수량'의 합계
       Total = df['일평균 재고수량'].sum()
-
-      #df['비율(%)'] = df['일평균 재고수량']/'Total'
       df['비율'] = df['일평균 재고수량']/Total
 
     if self.string_type == "release":
@@ -54,6 +53,13 @@ class ABC_analysis:
       df = df.sort_values(by='평균 출고금액',ascending=False)
       Total = df['평균 출고금액'].sum()
       df['비율'] = df['평균 출고금액']/Total
+
+    if self.string_type == "order":
+      #내림차순
+      df = df.sort_values(by='월평균 주문',ascending=False)
+      #'일평균 재고수량'의 합계
+      Total = df['월평균 주문'].sum()
+      df['비율'] = df['월평균 주문']/Total
     return df
 
 
@@ -79,6 +85,9 @@ class ABC_analysis:
 
     if self.string_type == "release":
       df['release level'] = df['누계비율(%)'].apply(self.func)
+
+    if self.string_type == "order":
+      df['order level'] = df['누계비율(%)'].apply(self.func)
     return df
 
   def pivot_data(self, df):
@@ -90,20 +99,37 @@ class ABC_analysis:
       df = df.groupby(['release level'])[['release level']].count()
       df_pivot = df.transpose()
 
+    if self.string_type == "order":
+      df = df.groupby(['order level'])[['order level']].count()
+      df_pivot = df.transpose()
+
     return df_pivot
 
   def join_data(self, stock_df, release_df):
-    df_join = stock_df.join(release_df.set_index('코드')['release level'], on='코드')
+    if self.string_type == "release":
+      df_join = stock_df.join(release_df.set_index('코드')['release level'], on='코드')
+
+    if self.string_type == "order":
+      df_join = stock_df.join(release_df.set_index('코드')['order level'], on='코드')
     return df_join
+
+    df_ABC통합1=df_ABC통합.join(d.set_index('코드')['order level'], on='코드')
 
   def wanted_data(self, df):
     df = df.loc[:,['코드', '단위','stock level', 'release level']]
     return df
 
   def final_pivot_data(self, df):
-    df_1 = pd.pivot_table(df, index = ['stock level'], columns = ['release level'], values = ['코드'],aggfunc =['count'])
-    df_1_p = df_1.rename(columns={'코드':'code'})
+    if self.string_type == "release":
+      df_1 = pd.pivot_table(df, index = ['stock level'], columns = ['release level'], values = ['코드'],aggfunc =['count'])
+      df_1_p = df_1.rename(columns={'코드':'code'})
 
-    df_1_p = df_1_p.reset_index()
+      df_1_p = df_1_p.reset_index()
 
+    if self.string_type == "order":
+      df_1 = pd.pivot_table(df, index = ['stock level'], columns = ['release level','order level'], values = ['코드'],aggfunc =['count'])
+      df_1_p = df_1.fillna(0)
+
+      df_1_p=df_1_p.rename(columns={'코드':'code'})
+      df_1_p = df_1_p.astype('int').reset_index()
     return df_1_p
